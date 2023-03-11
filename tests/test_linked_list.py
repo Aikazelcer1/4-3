a4ba2a08492afe4261ed5730131a1320e5d9814c
Aikazelcer1 <asya146@mail.ru>
2023-03-11 21:37:24 +0300

    new

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,78 +1,67 @@
-import contextlib
-import io
 import unittest
 
 from datastruct.linked_list import LinkedList
 
 
-class TestLinkedList(unittest.TestCase):
+class TestLinkedTest(unittest.TestCase):
+
     def setUp(self):
 
-        self.linked_list = LinkedList()
+        self.ll = LinkedList()
+
+        self.ll2 = LinkedList()
+        self.ll2.insert_beginning({'id': 1})
+        self.ll2.insert_at_end({'id': 2})
+        self.ll2.insert_at_end({'id': 3})
+        self.ll2.insert_beginning({'id': 0})
 
     def test_empty_linked_list(self):
 
-        self.assertEqual(self.linked_list.first_node, None)
-        self.assertEqual(self.linked_list.last_node, None)
+        self.assertEqual(self.ll.head, None)
+        self.assertEqual(self.ll.tail, None)
+        self.assertEqual(self.ll.print_ll_(), None)
 
-    def test_add_one_element_to_the_beginning(self):
+    def test_insert_beginning_element(self):
 
-        self.linked_list.insert_beginning(data=1)
+        self.ll.insert_beginning({'id': 1})
+        self.assertEqual(self.ll.head.data, {'id': 1})
+        self.assertEqual(self.ll.tail.data, {'id': 1})
+        self.assertEqual(self.ll.head.next_node, None)
+        self.assertEqual(self.ll.tail.next_node, None)
 
-        self.assertEqual(self.linked_list.first_node.data, 1)
-        self.assertEqual(self.linked_list.last_node.data, 1)
-        self.assertEqual(str(self.linked_list), '1 -> None')
+    def test_insert_at_end_element(self):
 
-    def test_add_one_element_to_the_end(self):
+        self.ll.insert_at_end({'id': 1})
+        self.assertEqual(self.ll.head.data, {'id': 1})
+        self.assertEqual(self.ll.tail.data, {'id': 1})
+        self.assertEqual(self.ll.head.next_node, None)
+        self.assertEqual(self.ll.tail.next_node, None)
 
-        self.linked_list.insert_at_end(data=1)
+    def test_insert_incorrect_element(self):
 
-        self.assertEqual(self.linked_list.first_node.data, 1)
-        self.assertEqual(self.linked_list.last_node.data, 1)
-        self.assertEqual(str(self.linked_list), '1 -> None')
+        result = 'Данные не являются словарем или в словаре нет id.'
+        self.assertEqual(self.ll.insert_beginning([1, 2, 3]), print(result))
+        self.assertEqual(self.ll.insert_at_end({'one': 1}), print(result))
 
-    def test_add_elements_to_the_beginning_and_end(self):
+    def test_check_linked_list(self):
 
-        self.linked_list.insert_beginning({'id': 1})
-        self.linked_list.insert_at_end({'id': 2})
-        self.linked_list.insert_at_end({'id': 3})
-        self.linked_list.insert_beginning({'id': 0})
+        self.assertEqual(self.ll2.head.data, {'id': 0})
+        self.assertEqual(self.ll2.tail.data, {'id': 3})
+        self.assertEqual(self.ll2.head.next_node.data, {'id': 1})
+        self.assertEqual(self.ll2.head.next_node.next_node.data, {'id': 2})
+        self.assertEqual(self.ll2.head.next_node.next_node.next_node.data, {'id': 3})
+        self.assertEqual(self.ll2.tail.next_node, None)
 
-        self.assertEqual(self.linked_list.first_node.data, {'id': 0})
-        self.assertEqual(self.linked_list.first_node.next_node.data, {'id': 1})
-        self.assertEqual(self.linked_list.last_node.data, {'id': 3})
-        self.assertEqual(self.linked_list.last_node.next_node, None)
-        self.assertEqual(str(self.linked_list), "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None")
+    def test_print_ll(self):
+
+        self.assertEqual(self.ll2.print_ll_(), "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None")
 
     def test_to_list(self):
 
-        self.linked_list.insert_beginning({'id': 1, 'username': 'lazzy508509'})
-        self.linked_list.insert_at_end({'id': 2, 'username': 'mik.roz'})
-        self.linked_list.insert_at_end({'id': 3, 'username': 'mosh_s'})
-        self.linked_list.insert_beginning({'id': 0, 'username': 'serebro'})
+        self.assertEqual(self.ll.to_list(), None)
+        self.assertIsInstance(self.ll2.to_list(), list)
 
-        self.assertEqual(len(self.linked_list.to_list()), 4)
+    def test_get_data_by_id(self):
 
-    def test_get_data_by_id_correct_data(self):
-
-        self.linked_list.insert_beginning({'id': 1, 'username': 'lazzy508509'})
-        self.linked_list.insert_at_end({'id': 2, 'username': 'mik.roz'})
-        self.linked_list.insert_at_end({'id': 3, 'username': 'mosh_s'})
-        self.linked_list.insert_beginning({'id': 0, 'username': 'serebro'})
-
-        self.assertEqual(self.linked_list.get_data_by_id(value_id=3), {'id': 3, 'username': 'mosh_s'})
-        self.assertEqual(self.linked_list.get_data_by_id(value_id=4), None)
-
-    def test_get_data_by_id_incorrect_data(self):
-
-        self.linked_list.insert_beginning({'id': 1, 'username': 'lazzy508509'})
-        self.linked_list.insert_at_end('idusername')
-        self.linked_list.insert_at_end([1, 2, 3])
-        self.linked_list.insert_at_end({'id': 2, 'username': 'serebro'})
-
-        self.assertEqual(self.linked_list.get_data_by_id(value_id=2), None)
-
-        s = io.StringIO()
-        with contextlib.redirect_stdout(s):
-            self.linked_list.get_data_by_id(value_id=2)
-        self.assertEqual(s.getvalue(), 'Данные не являются словарем или в словаре нет id.\n')
+        self.assertEqual(self.ll2.get_data_by_id(3), {'id': 3})
+        self.assertEqual(self.ll2.get_data_by_id(4), None)
